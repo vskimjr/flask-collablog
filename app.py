acd24c2d90ba9f25c9bb67548f1409304b9203e2
Victor Kim <vskimjr@gmail.com>
2024-04-17 13:48:14 -0400

    added User class, added more dependencies

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///collablog')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'sUp37$EcREtK3Y-c0lL@bLOG'
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Collablog."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/img/1672"
+
+class User(db.Model):
+    """Collablog user"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    first_name = db.Column(
+        db.String(25),
+        nullabe=False
+    )
+
+    last_name = db.Column(
+        db.String(35),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=DEFAULT_IMAGE_URL
+    )
