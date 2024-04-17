fe5c1aa83fc08041dea4faf89d83338b258b9a99
Victor Kim <vskimjr@gmail.com>
2024-04-17 15:44:39 -0400

    edited user data to include about information

--- seed.py
+++ seed.py
@@ -8,14 +8,28 @@ def create_user_data():
     """Inserts test user data to collablog users table"""
 
     users = [
-        {"first_name": "Victor", "last_name": "Kim"},
-        {"first_name": "David", "last_name": "Jo"},
-        {"first_name": "Gilbert", "last_name": "Salas"},
+        {
+            "first_name": "Victor",
+            "last_name": "Kim",
+            "about": "I went to WTS"
+        },
+        {
+            "first_name": "David",
+            "last_name": "Jo",
+            "about": "I went to CUNY"
+        },
+        {
+            "first_name": "Gilbert",
+            "last_name": "Salas",
+            "about": "I went to CUNY"
+        },
     ]
 
     for user_data in users:
 
-        # Double asterist unpacks a dictionary into keyword arguments in function call
+        # Double asterisks unpacks a dictionary
+        # into keyword arguments in function call
+
         user = User(**user_data)
         db.session.add(user)
 
